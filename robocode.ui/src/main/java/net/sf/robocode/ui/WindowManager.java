7b082e8409c85301c109c9b5564910cb5a424330
bumfo <bumfo@users.noreply.github.com>
2019-12-24 21:34:49 +0800

    updateRestartButtonStatus

--- robocode.ui/src/main/java/net/sf/robocode/ui/WindowManager.java
+++ robocode.ui/src/main/java/net/sf/robocode/ui/WindowManager.java
@@ -497,6 +497,8 @@ public class WindowManager implements IWindowManagerExt {
 			WindowUtil.packCenterShow(getRobocodeFrame(), battleDialog);
 		} finally {
 			battleManager.resumeBattle();
+
+			robocodeFrame.updateRestartButtonStatus();
 		}
 	}
 
