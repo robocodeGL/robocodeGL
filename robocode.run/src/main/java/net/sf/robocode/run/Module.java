d3dd4065e8caee46deab8f5b0534bbaa6014bc43
bumfo <bumfo@users.noreply.github.com>
2020-01-06 21:25:39 +0800

    module robocode.run for running robocode from IDE

---
+++ robocode.run/src/main/java/net/sf/robocode/run/Module.java
@@ -0,0 +1,21 @@
+/**
+ * Copyright (c) 2001-2019 Mathew A. Nelson and Robocode contributors
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * https://robocode.sourceforge.io/license/epl-v10.html
+ */
+package net.sf.robocode.run;
+
+
+import net.sf.robocode.core.Container;
+
+
+/**
+ * @author Pavel Savara (original)
+ */
+public class Module {
+	static {
+		// Container.cache.addComponent(IGL2.class, GL2.class);
+	}
+}
