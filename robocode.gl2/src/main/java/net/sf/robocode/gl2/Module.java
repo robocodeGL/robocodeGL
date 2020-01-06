0159c7842cd61aefecd6674214e0ab99fe6f9783
bumfo <bumfo@users.noreply.github.com>
2020-01-06 20:03:04 +0800

    update license

--- robocode.gl2/src/main/java/net/sf/robocode/gl2/Module.java
+++ robocode.gl2/src/main/java/net/sf/robocode/gl2/Module.java
@@ -1,14 +1,10 @@
-/*******************************************************************************
- * Copyright (c) 2001, 2008 Mathew A. Nelson and Robocode contributors
+/**
+ * Copyright (c) 2001-2019 Mathew A. Nelson and Robocode contributors
  * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the Common Public License v1.0
+ * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
- * http://robocode.sourceforge.net/license/cpl-v10.html
- *
- * Contributors:
- *     Pavel Savara
- *     - Initial implementation
- *******************************************************************************/
+ * https://robocode.sourceforge.io/license/epl-v10.html
+ */
 package net.sf.robocode.gl2;
 
 
