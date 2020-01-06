ae04bd4ec2f4e5c198d5559f7cc02ac657a9a1d0
bumfo <bumfo@users.noreply.github.com>
2020-01-06 17:18:59 +0800

    make robocode.bat setting up current directory correctly

--- robocode.content/src/main/resources/robocode.bat
+++ robocode.content/src/main/resources/robocode.bat
@@ -6,4 +6,5 @@
 @REM https://robocode.sourceforge.io/license/epl-v10.html
 @REM
 
+cd /D "%~dp0"
 java -Xmx512M -cp libs/robocode.jar -XX:+IgnoreUnrecognizedVMOptions "--add-opens=java.base/sun.net.www.protocol.jar=ALL-UNNAMED" "--add-opens=java.base/java.lang.reflect=ALL-UNNAMED" "--add-opens=java.desktop/javax.swing.text=ALL-UNNAMED" "--add-opens=java.desktop/sun.awt=ALL-UNNAMED" robocode.Robocode %*
