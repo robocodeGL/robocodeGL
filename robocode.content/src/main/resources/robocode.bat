48e7024048c93c03e3f7d913f159cfb1bfc9b29f
bumfo <bumfo@users.noreply.github.com>
2020-01-09 15:17:26 +0800

    remove illegal reflective access warnings

--- robocode.content/src/main/resources/robocode.bat
+++ robocode.content/src/main/resources/robocode.bat
@@ -7,5 +7,5 @@
 @REM
 
 @pushd "%~dp0"
-java -Xmx512M -cp libs/robocode.jar -XX:+IgnoreUnrecognizedVMOptions "--add-opens=java.base/sun.net.www.protocol.jar=ALL-UNNAMED" "--add-opens=java.base/java.lang.reflect=ALL-UNNAMED" "--add-opens=java.desktop/javax.swing.text=ALL-UNNAMED" "--add-opens=java.desktop/sun.awt=ALL-UNNAMED" robocode.Robocode %*
+java -Xmx512M -cp libs/robocode.jar -XX:+IgnoreUnrecognizedVMOptions "--add-opens=java.base/sun.net.www.protocol.jar=ALL-UNNAMED" "--add-opens=java.base/java.lang.reflect=ALL-UNNAMED" "--add-opens=java.desktop/javax.swing.text=ALL-UNNAMED" "--add-opens=java.desktop/sun.awt=ALL-UNNAMED" "--add-opens=java.desktop/java.awt=ALL-UNNAMED" "--add-opens=java.base/java.lang=ALL-UNNAMED" "--add-opens=java.desktop/sun.java2d.opengl=ALL-UNNAMED" "--add-opens=java.desktop/sun.awt.windows=ALL-UNNAMED" robocode.Robocode %*
 @popd
